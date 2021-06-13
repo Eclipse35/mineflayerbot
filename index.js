3d1cba1a8dee77874b2af8c2954668b5c26f4661
Eclipse35 <75852840+Eclipse35@users.noreply.github.com>
2021-06-13 13:55:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const armorManager = require('mineflayer-armor-manager')
 const bot = mineflayer.createBot({
     host: '109.236.91.2',
     port:  43001,
-    username: 'Bot35',
+    username: '_F5_',
     version: false,
 //    password: process.argv[5],
 })
